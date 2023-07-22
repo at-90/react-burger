01d56350979e5ec6d2e9ecb75e83b2c1f22f3086
at-90 <64415067+at-90@users.noreply.github.com>
2023-07-22 10:43:13 +0300

    refactor: e2e options

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,8 +9,11 @@ export default defineConfig({
   },
 
   e2e: {
+
+    baseUrl: 'http://localhost:3000/',
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
     },
   },
 });
--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,42 +1,45 @@
+const cardSelector = '[class^=card_card]';
+const construcorSelector = '[class^=burger-constructor_order__]';
+
 describe("Тест drag and drop и модальных окон в конструкторе", () => {
 
     beforeEach(() => {
-        cy.visit("http://localhost:3000/");
+        cy.visit("/");
         cy.viewport(1920, 1080);
     });
 
     it('Открывается модальное окно при клике на ингредиент', function () {
 
-        cy.get('[class^=card_card]').first().as('ingredient').click();
+        cy.get(cardSelector).first().as('ingredient').click();
         cy.get('[class^=modal_]').as('modal');
         cy.get('@modal').find('[class^=modal_btnClose]').click();
     });
 
     it("Ингредиенты перетаскивается", () => {
 
-        cy.get('[class^=card_card]').contains('булка').first().as('bulk');
-        cy.get('[class^=card_card]').contains('Соус').first().as('ingredient');
+        cy.get(cardSelector).contains('булка').first().as('bulk');
+        cy.get(cardSelector).contains('Соус').first().as('ingredient');
         cy.get('@bulk').trigger("dragstart");
-        cy.get('[class^=burger-constructor_order__]').trigger("drop");
+        cy.get(construcorSelector).trigger("drop");
         cy.get('@ingredient').trigger("dragstart");
-        cy.get('[class^=burger-constructor_order__]').trigger("drop");
+        cy.get(construcorSelector).trigger("drop");
 
     });
 
     it("Заказ создан", () => {
 
-        cy.get('[class^=card_card]').contains('булка').first().as('bulk');
-        cy.get('[class^=card_card]').contains('Соус').first().as('ingredient');
+        cy.get(cardSelector).contains('булка').first().as('bulk');
+        cy.get(cardSelector).contains('Соус').first().as('ingredient');
         cy.get('@bulk').trigger("dragstart");
-        cy.get('[class^=burger-constructor_order__]').trigger("drop");
+        cy.get(construcorSelector).trigger("drop");
         cy.get('@ingredient').trigger("dragstart");
-        cy.get('[class^=burger-constructor_order__]').trigger("drop");
+        cy.get(construcorSelector).trigger("drop");
         cy.get('button').contains('Оформить заказ').click();
         cy.wait(700);
         cy.get('form [type=email]').type('124888@ya.ru');
         cy.get('form [type=password]').type('111111');
         cy.get('form [type=submit]').click();
-        cy.get('[class^=card_card]').contains('булка').first().as('bulk');
+        cy.get(cardSelector).contains('булка').first().as('bulk');
         cy.get('button').contains('Оформить заказ').click();
         cy.wait(20000);
         cy.get("body").type("{esc}");
